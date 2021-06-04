8bc2aaf743d2682d86d243ca8234e2137638e586
teerasuk55 <teerasukmukpiboon@gmail.com>
2021-06-04 12:15:17 +0700

    change layout

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,11 +1,11 @@
-﻿$(document).ready(function () {
-    if(window.location.pathname != '/' && 
-     window.location.pathname != "/Home" &&
-     window.location.pathname != "/Home/Index"){
-        $('header').show();
-        $('footer').show() 
-    }else{
-        $('header').empty();
-        $('footer').empty()
-    }
-});
+﻿// $(document).ready(function () {
+//     if(window.location.pathname != '/' && 
+//      window.location.pathname != "/Home" &&
+//      window.location.pathname != "/Home/Index"){
+//         $('header').show();
+//         $('footer').show() 
+//     }else{
+//         $('header').empty();
+//         $('footer').empty()
+//     }
+// });
